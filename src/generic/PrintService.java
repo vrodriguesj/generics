f62bf6f6ed1603edbdec48fc946fe60c8b9f8c14
Vinicius Rodrigues <vrodrigues2112@gmail.com>
2020-04-01 13:25:46 -0300

    Projeto Criado

---
+++ src/generic/PrintService.java
@@ -0,0 +1,32 @@
+package generic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintService<T> {
+
+	private List<T> list = new ArrayList<>();
+
+	public void addValue(T value) {
+		list.add(value);
+	}
+
+	public T first() {
+		if (list.isEmpty()) {
+			throw new IllegalStateException("Lista vazia");
+		}
+		return list.get(0);
+	}
+	
+	public void printf() {
+		System.out.print("[");
+		if (!list.isEmpty()) {
+			System.out.print(list.get(0));
+		}
+		for (int i=0; i < list.size(); i++) {
+			System.out.print(", " + list.get(i));
+		}
+		System.out.print("] ");
+	}
+
+}
